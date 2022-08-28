fdb55c1d9aa6110d6e76418e1673b49f5b9a237c
mohosin <mohosin@gmail.com>
2022-08-28 13:05:43 +0600

    al least successful

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-/**
- * Project Requirements:
- * - Change the background color by generating random rbg color by clicking a button
- */
-
-// Steps
-
-// Step 1 - create onload handler
 window.onload = () => {
   main();
 };
@@ -20,16 +12,10 @@ function main() {
   });
 }
 
-// step 2 - random color generator function
 function generateRGBColor() {
-  // rgb(0, 0, 0), rgb(255, 255, 255)
   const red = Math.floor(Math.random() * 255);
   const green = Math.floor(Math.random() * 255);
   const blue = Math.floor(Math.random() * 255);
 
   return `rgb(${red}, ${green}, ${blue})`;
 }
-
-// step 3 - collect all necessary references
-
-// step 4 - handle the click event
