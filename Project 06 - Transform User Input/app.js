7643ec81b9e5cc80fc89f9a92353bcc438120232
mohosin <mohosin@gmail.com>
2022-09-14 19:06:13 +0600

    I completed all task

---
+++ Project 06 - Transform User Input/app.js
@@ -0,0 +1,110 @@
+/**
+ * Project Requirements:
+ * - Change the background color by generating random hex color by clicking a button
+ * - Also display the hex code to a disabled input field
+ * - Add a button to copy the color code
+ * - Add a toast message when copied
+ * - User can type their own hex code too
+ */
+
+/**
+ * Steps
+ * 1. create onload handler
+ * 2. random color generator function
+ * 3. collect all necessary dom references
+ * 4. handle the change button click event
+ * 5. handle the copy button click event
+ * 6. activate toast message
+ * 7. create a dynamic toast message
+ * 8. clear toast message
+ * 9. create isValidHex function
+ * 10. implement change handler on input field
+ * 11. prevent copying hex code if it is not valid
+ */
+
+// Globals
+let div = null;
+
+// onload handler
+window.onload = () => {
+  main();
+};
+
+function main() {
+  // dom references
+  const root = document.getElementById("root");
+  const output = document.getElementById("output");
+  const changeBtn = document.getElementById("change-btn");
+  const copyBtn = document.getElementById("copy-btn");
+
+  // change button event listener & handler
+  changeBtn.addEventListener("click", function () {
+    const bgColor = generateHexColor();
+    root.style.backgroundColor = bgColor;
+    output.value = bgColor.substring(1);
+  });
+
+  // copy button event listener & handler
+  copyBtn.addEventListener("click", function () {
+    navigator.clipboard.writeText(`#${output.value}`);
+    if (div !== null) {
+      div.remove();
+      div = null;
+    }
+
+    // prevent copying invalid color code
+    if (isValidHex(output.value)) {
+      generateToastMessage(`#${output.value} copied`);
+    } else {
+      alert("Invalid color code");
+    }
+  });
+
+  // input field change handler
+  output.addEventListener("keyup", function (e) {
+    const color = e.target.value;
+    if (color) {
+      output.value = color.toUpperCase();
+      if (isValidHex(color)) {
+        root.style.backgroundColor = `#${color}`;
+      }
+    }
+  });
+}
+
+// random color generator function
+function generateHexColor() {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+
+  return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+}
+
+// activate or generate a toast message
+function generateToastMessage(msg) {
+  div = document.createElement("div");
+  div.innerText = msg;
+  div.className = "toast-message toast-message-slide-in";
+
+  div.addEventListener("click", function () {
+    div.classList.remove("toast-message-slide-in");
+    div.classList.add("toast-message-slide-out");
+
+    div.addEventListener("animationend", function () {
+      div.remove();
+      div = null;
+    });
+  });
+
+  document.body.appendChild(div);
+}
+
+/**
+ * isValidHex function
+ * @param {string} color
+ */
+function isValidHex(color) {
+  if (color.length !== 6) return false;
+  return /^[0-9A-Fa-f]{6}$/i.test(color);
+}
---
+++ Project 07 - Working With Both Hexadecimal And RGB Color Code/app.js
@@ -0,0 +1,148 @@
+/**
+ * Project Requirements:
+ * - Change the background color by generating random hex color by clicking a button
+ * - Also display the hex code to a disabled input field
+ * - Add a button to copy the color code
+ * - Add a toast message when copied
+ * - User can type their own hex code too
+ * - show rgb color too, but do not need to edit it
+ * - user can also copy the rgb color code
+ */
+
+/**
+ * Steps
+ * 1. create onload handler
+ * 2. random color generator function
+ * 3. collect all necessary dom references
+ * 4. handle the change button click event
+ * 5. handle the copy button click event
+ * 6. activate toast message
+ * 7. create a dynamic toast message
+ * 8. clear toast message
+ * 9. create isValidHex function
+ * 10. implement change handler on input field
+ * 11. prevent copying hex code if it is not valid
+ * 12. refactor the color generator function
+ * 13. update color code to display rbg colors
+ */
+
+// Globals
+let div = null;
+
+// onload handler
+window.onload = () => {
+  main();
+};
+
+function main() {
+  // dom references
+  const root = document.getElementById("root");
+  const output = document.getElementById("output");
+  const output2 = document.getElementById("output2");
+  const changeBtn = document.getElementById("change-btn");
+  const copyBtn = document.getElementById("copy-btn");
+  const copyBtn2 = document.getElementById("copy-btn2");
+
+  // change button event listener & handler
+  changeBtn.addEventListener("click", function () {
+    const color = generateColorDecimal();
+    const hex = generateHexColor(color);
+    const rgb = generateRGBColor(color);
+    root.style.backgroundColor = hex;
+    output.value = hex.substring(1);
+    output2.value = rgb;
+  });
+
+  // copy button event listener & handler
+  copyBtn.addEventListener("click", function () {
+    navigator.clipboard.writeText(`#${output.value}`);
+    if (div !== null) {
+      div.remove();
+      div = null;
+    }
+    // prevent copying invalid color code
+    if (isValidHex(output.value)) {
+      generateToastMessage(`#${output.value} copied`);
+    } else {
+      alert("Invalid color code");
+    }
+  });
+
+  copyBtn2.addEventListener("click", function () {
+    navigator.clipboard.writeText(output2.value);
+    if (div !== null) {
+      div.remove();
+      div = null;
+    }
+    generateToastMessage(`${output2.value} copied`);
+  });
+
+  // input field change handler
+  output.addEventListener("keyup", function (e) {
+    const color = e.target.value;
+    if (color) {
+      output.value = color.toUpperCase();
+      if (isValidHex(color)) {
+        root.style.backgroundColor = `#${color}`;
+      }
+    }
+  });
+}
+
+/**
+ * function - 01
+ * generate three random decimal number for red, green and blue
+ * @returns as a object
+ */
+function generateColorDecimal() {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+
+  return { red, green, blue };
+}
+
+// function 2 - generate hex color code
+function generateHexColor({ red, green, blue }) {
+  const getTwoCode = (value) => {
+    const hex = value.toString(16);
+    return hex.length === 1 ? `0${hex}` : hex;
+  };
+
+  return `#${getTwoCode(red)}${getTwoCode(green)}${getTwoCode(
+    blue
+  )}`.toUpperCase();
+}
+
+// function 3 - generate rgba color code
+function generateRGBColor({ red, green, blue }) {
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+
+// activate or generate a toast message
+function generateToastMessage(msg) {
+  div = document.createElement("div");
+  div.innerText = msg;
+  div.className = "toast-message toast-message-slide-in";
+
+  div.addEventListener("click", function () {
+    div.classList.remove("toast-message-slide-in");
+    div.classList.add("toast-message-slide-out");
+
+    div.addEventListener("animationend", function () {
+      div.remove();
+      div = null;
+    });
+  });
+
+  document.body.appendChild(div);
+}
+
+/**
+ * isValidHex function
+ * @param {string} color
+ */
+function isValidHex(color) {
+  if (color.length !== 6) return false;
+  return /^[0-9A-Fa-f]{6}$/i.test(color);
+}
